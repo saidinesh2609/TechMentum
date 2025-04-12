93759fed8449493fdcb6da67cde4ba311696b5c9
Sai Dinesh <saidinesh@Sais-Laptop.local>
2025-04-13 03:08:01 +0530

    modified files

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -1,6 +1,5 @@
 // netlify/functions/chat.js
 const { OpenAI } = require('openai');
-
 require("dotenv").config();
 
 const openai = new OpenAI({
@@ -8,11 +7,24 @@ const openai = new OpenAI({
 });
 
 exports.handler = async function(event) {
+  // Handle CORS preflight request
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': 'https://techmentum.in',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: 'Preflight OK',
+    };
+  }
+
   if (event.httpMethod !== 'POST') {
     return {
       statusCode: 405,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': 'https://techmentum.in',
         'Access-Control-Allow-Headers': 'Content-Type',
       },
       body: JSON.stringify({ error: 'Method Not Allowed' }),
@@ -32,7 +44,7 @@ exports.handler = async function(event) {
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': 'https://techmentum.in',
         'Access-Control-Allow-Headers': 'Content-Type',
       },
       body: JSON.stringify({ reply }),
@@ -42,10 +54,11 @@ exports.handler = async function(event) {
     return {
       statusCode: 500,
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': 'https://techmentum.in',
         'Access-Control-Allow-Headers': 'Content-Type',
       },
       body: JSON.stringify({ error: "Failed to get response from OpenAI" }),
     };
   }
 };
+
